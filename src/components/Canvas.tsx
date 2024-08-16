eb509d021620ccaa4753b5139c881f85bd57cf15
rorychatt <pavel.rinne@gmail.com>
2024-08-16 13:47:42 +0200

    Feat: basic cube rendered

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-﻿import { useEffect, useRef } from 'react';
+﻿import {useEffect, useRef} from 'react';
 
 const Canvas = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -8,13 +8,13 @@ const Canvas = () => {
         const canvas = canvasRef.current;
         if (canvas && !transferredRef.current) {
             const offscreen = canvas.transferControlToOffscreen();
-            const worker = new Worker(new URL('../ts/threeWorker.ts', import.meta.url), { type: 'module' });
-            worker.postMessage({ canvas: offscreen }, [offscreen]);
+            const worker = new Worker(new URL('../ts/threeWorker.ts', import.meta.url), {type: 'module'});
+            worker.postMessage({canvas: offscreen}, [offscreen]);
             transferredRef.current = true;
         }
     }, []);
 
-    return <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />;
+    return <canvas ref={canvasRef} style={{width: '100%', height: '100%'}}/>;
 };
 
 export default Canvas;
