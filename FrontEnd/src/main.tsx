57636c94b0f132e88069de2e65ddc8195a7d2e61
rorychatt <pavel.rinne@gmail.com>
2024-08-28 17:28:10 +0200

    Feat: router setup

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+import {createRouter, RouterProvider} from "@tanstack/react-router";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key')
+}
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      TanStackRouterVite(),
+      react()],
 })
